cc9f4eccb179c17617ddf087a218da2851b6f01b
alexis-022 <me@alexishood.net>
2026-04-26 15:38:37 +1200

    26/4/26 - Read Listener + Complex Write

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-/**************************************************************
- **************************************************************
- **                                                          **
- ** script.js is where you will write most of your code.     **
- **                                                          **
- **************************************************************
- **************************************************************/
 
-const HTML_OUTPUT = document.getElementById("databaseOutput");
+/**                                                          **
+ ** script.js is where you will write most of your code.     **
+ **                                                          **/
 
 /**************************************************************/
 // helloWorld()
@@ -18,19 +13,41 @@ const HTML_OUTPUT = document.getElementById("databaseOutput");
 // This means it replaces the whole database with message:Hello World
 /**************************************************************/
 
-function helloWorld() {
-  console.log("Running helloWorld()")
-  firebase.database().ref('/').set(
-    {
-      message: 'Hello World!'
-    }
-  )
+var highScoreTable = {
+  users: {
+    Alexis: 100,
+    Blake: 20,
+    Wilkin: 200
+  }
+}
+
+
+function simpleWrite() {
+  console.log("Writing Message")
+  firebase.database().ref('/messages').set({ message: 'Hello World!' })
 }
 
 function simpleRead() {
   console.log("Reading Message")
-  firebase.database().ref('/message').once('value', displayRead, fb_readError)
+  firebase.database().ref('/messages/message').once('value', displayRead, fb_readError) // .on for listen for change, .once for once
 }
+
+function readListener() {
+  console.log("Reading Message with Listener")
+  firebase.database().ref('/messages/message').on('value', displayRead, fb_readError) // .on for listen for change, .once for once
+}
+
+function complexWrite() {
+  console.log("Writing High Score Table")
+  firebase.database().ref('/highscoretable').set(highScoreTable)
+}
+
+function updateWrite(user, score) {
+  console.log("Updating High Score User Alexis")
+  firebase.database().ref('/highscoretable/users/'+user).set(score)
+}
+
+//Resilient code stuff:
 
 function displayRead(snapshot) {
   var dbData = snapshot.val();
@@ -38,10 +55,12 @@ function displayRead(snapshot) {
     console.log("There was no record when trying to read from the database!");
   } else {
     console.log("The message is: " + dbData);
+    databaseOutput.textContent = dbData;
+
   }
 }
 
-function fb_readError(error){
+function fb_readError(error) {
   console.log("There was an error reading this message!")
   console.error(error);
 }
